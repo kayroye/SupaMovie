56e6ca33d16e12c904608f8da82f9a2efab53884
Kay Roye <05kalusss@gmail.com>
2025-12-27 19:44:28 -0500

    Initial Commit

---
+++ SupaMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SupaMovie
+//
+//  Created by Kay Roye on 2025-12-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SupaMovie/SupaMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupaMovieApp.swift
+//  SupaMovie
+//
+//  Created by Kay Roye on 2025-12-27.
+//
+
+import SwiftUI
+
+@main
+struct SupaMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
